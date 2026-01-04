0ee21d65ac7c2b6059975492a0ece10702a2285c
Pavan Kalyan <pavankalyanpula@gmail.com>
2026-01-04 22:00:31 +0530

    Time: 4 ms (99.72%) | Memory: 47.4 MB (91.22%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static boolean palind(String s,int l,int r){
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    
+    public boolean validPalindrome(String s) {
+       int l=0;
+       int r=s.length()-1;
+       while(l<r){
+        if(s.charAt(l)!=s.charAt(r)){
+            return palind(s,l+1,r) || palind(s,l,r-1);
+        }
+        l++;
+        r--;
+       }
+       return true;
+    }
+}
