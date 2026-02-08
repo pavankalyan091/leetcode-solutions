0195f80a253ed6ef77492adee284aae8bcffa7c3
Pavan Kalyan <pavankalyanpula@gmail.com>
2026-02-08 22:16:20 +0530

    Time: 3 ms (78.29%) | Memory: 46.7 MB (65.17%) - LeetSync

--- PREFIX_SUM_PATTERN/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ PREFIX_SUM_PATTERN/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,19 +1,30 @@
 class Solution {
+
+    public static boolean isVowel(char c){
+        c=Character.toLowerCase(c);
+        return (c=='a'|| c=='e'|| c=='i'|| c=='o'||c=='u');
+    }
     public String reverseVowels(String s) {
-        HashSet<Character>hs =new HashSet<>(Arrays.asList('a','e','i','o','u','A','E','I','O','U'));
+        // HashSet<Character>hs =new HashSet<>(Arrays.asList('a','e','i','o','u','A','E','I','O','U'));
         
         char arr[]=s.toCharArray();
         int l=0;
         int r=arr.length-1;
         while(l<r){
-            while(l<r && !hs.contains(arr[l])){
+            // while(l<r && !hs.contains(arr[l])){
+            //     l++;
+            // }
+
+            // while(l<r && !hs.contains(arr[r])){
+            //     r--;
+            // }
+
+            while (l<r && !isVowel(arr[l])){
                 l++;
             }
-
-            while(l<r && !hs.contains(arr[r])){
+            while(l<r && !isVowel(arr[r])){
                 r--;
             }
-
             char temp=arr[l];
             arr[l]=arr[r];
             arr[r]=temp;
